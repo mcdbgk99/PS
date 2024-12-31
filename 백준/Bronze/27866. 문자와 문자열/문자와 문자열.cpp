e8efe8a09bc135ba97345c1f8b000957638ff82d
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-01 02:18:44 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2088 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string str;
+  cin >> str;
+  int n;
+  cin >> n;
+  cout << str[n - 1];
+
+  return 0;
+}
