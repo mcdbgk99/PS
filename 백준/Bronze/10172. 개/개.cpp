6edc5b6299b9bf41a0a554e9fb7bf8fb0e1716bd
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-01 01:00:14 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string str =
+      "|\\_/|\n"
+      "|q p|   /}\n"
+      "( 0 )\"\"\"\\\n"
+      "|\"^\"`    |\n"
+      "||_/=\\\\__|\n";
+  cout << str;
+
+  return 0;
+}
