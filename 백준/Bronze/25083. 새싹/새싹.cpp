ab6d894f11c4530159fd922ea102e950b014d2cf
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-01 02:18:38 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string str =
+      "         ,r'\"7\n"
+      "r`-_   ,'  ,/\n"
+      " \\. \". L_r'\n"
+      "   `~\\/\n"
+      "      |\n"
+      "      |";
+  cout << str;
+
+  return 0;
+}
