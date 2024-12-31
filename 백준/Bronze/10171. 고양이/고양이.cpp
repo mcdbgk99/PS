ef2cd0bbdac52547b80914c65f9cbfef5c0b2f35
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-01 01:00:07 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string str = R"(\    /\)"
+               "\n"
+               R"( )  ( '))"
+               "\n"
+               R"((  /  ))"
+               "\n"
+               R"( \(__)|)"
+               "\n";
+  cout << str;
+
+  return 0;
+}
