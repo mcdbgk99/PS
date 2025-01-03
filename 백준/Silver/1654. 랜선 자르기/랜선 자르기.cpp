07d6ceaf7415a7e7716e48286e4f1996beabdbc7
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-04 07:20:27 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2280 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int64_t> v;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t k, n;
+  cin >> k >> n;
+
+  v.resize(k);
+
+  int64_t longest = 0;
+  for (int i = 0; i < k; ++i) {
+    cin >> v[i];
+    longest = max(longest, v[i]);
+  }
+
+  int64_t left = 1;
+  int64_t right = longest;
+  int64_t result = 0;
+
+  while (left <= right) {
+    int64_t mid = left + (right - left) / 2;
+
+    int64_t count = 0;
+    for (int i = 0; i < k; ++i) {
+      count += v[i] / mid;
+    }
+
+    if (count >= n) {
+      result = max(result, mid);
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  cout << result;
+
+  return 0;
+}
