2e92a91c080c6e5486f5a19e77d1374300345e80
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-07 11:54:46 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  v.resize(n + 1);
+  v[0] = 1;
+  v[1] = 1;
+
+  for (int i = 2; i <= n; ++i) {
+    v[i] = v[i - 2] + v[i - 1];
+    v[i] %= 10007;
+  }
+
+  cout << v[n];
+
+  return 0;
+}
