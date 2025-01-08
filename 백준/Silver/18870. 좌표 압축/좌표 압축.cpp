46bca7713911c0ca7081c4ec1e2630feccdaf611
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-09 02:05:16 +0900

    [Silver II] Title: 좌표 압축, Time: 1028 ms, Memory: 39608 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v;
+vector<int> temp;
+map<int, int> m;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  v.resize(n);
+  temp.resize(n);
+
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+    temp[i] = v[i];
+  }
+
+  sort(temp.begin(), temp.end());
+  temp.erase(unique(temp.begin(), temp.end()), temp.end());
+
+  for (int i = 0; i < temp.size(); ++i) {
+    m.insert({temp[i], i});
+  }
+
+  for (int i = 0; i < n; ++i) {
+    cout << m[v[i]] << " ";
+  }
+
+  return 0;
+}
