37e0d5b339aee4ff6ca5960e1da78e1f7ad520b0
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-09 16:13:18 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 24 ms, Memory: 2876 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v;
+queue<int> q;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, k;
+  cin >> n >> k;
+
+  v.resize(max(n, k) + 3);
+
+  v[n] = 1;
+  q.push(n);
+
+  int possible = 0;
+
+  while (!q.empty()) {
+    int now = q.front();
+    q.pop();
+
+    if (now == k) {
+      ++possible;
+    }
+
+    vector<int> nexts = {now + 1, now - 1, now * 2};
+    for (int next : nexts) {
+      if (next < 0 || next >= v.size()) {
+        continue;
+      }
+
+      if (v[next] != 0 && v[next] < v[now] + 1) {
+        continue;
+      }
+
+      v[next] = v[now] + 1;
+      q.push(next);
+    }
+  }
+
+  cout << v[k] - 1 << "\n" << possible;
+
+  return 0;
+}
