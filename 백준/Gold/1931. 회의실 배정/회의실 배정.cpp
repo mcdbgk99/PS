b67f19b707775eabd1ad02cd22ee5de2e23968e2
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-11 13:24:15 +0900

    [Gold V] Title: 회의실 배정, Time: 24 ms, Memory: 2872 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<tuple<int, int>> v;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  v.resize(n);
+
+  for (int i = 0; i < n; ++i) {
+    cin >> get<0>(v[i]) >> get<1>(v[i]);
+  }
+
+  sort(v.begin(), v.end(), [](tuple<int, int>& left, tuple<int, int>& right) {
+    return get<1>(left) > get<1>(right) ||
+           (get<1>(left) == get<1>(right) ? get<0>(left) > get<0>(right)
+                                          : false);
+  });
+  reverse(v.begin(), v.end());
+
+  int last_end = 0;
+  int result = 0;
+
+  for (auto& t : v) {
+    if (get<0>(t) >= last_end) {
+      ++result;
+      last_end = get<1>(t);
+    }
+  }
+
+  cout << result;
+
+  return 0;
+}
