72ee03b9434c65ebb49abb7dea762bb522266620
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-12 04:03:23 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 24 ms, Memory: 12516 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v(100 + 1, 0);
+vector<int> visited(100 + 1, 0);
+queue<tuple<int, int>> q;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+
+  for (int i = 0; i < n + m; ++i) {
+    int x;
+    cin >> x;
+    cin >> v[x];
+  }
+
+  q.push({1, 0});
+  int result = 0;
+
+  while (!q.empty()) {
+    int now_num = get<0>(q.front());
+    int now_step = get<1>(q.front());
+    q.pop();
+
+    if (now_num == 100) {
+      result = now_step;
+      break;
+    }
+
+    for (int i = 1; i <= 6; ++i) {
+      int new_num = now_num + i;
+      if (new_num > 100) {
+        continue;
+      }
+
+      if (v[new_num] != 0) {
+        new_num = v[new_num];
+      }
+
+      if ((visited[new_num] != 0 && visited[new_num] <= visited[now_num])) {
+        continue;
+      }
+
+      visited[new_num] = now_step + 1;
+      q.push({new_num, visited[new_num]});
+    }
+  }
+
+  cout << result;
+
+  return 0;
+}
