37e13835f65a5d94586b9a075985b5b96fa37999
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-12 12:56:06 +0900

    [Gold IV] Title: 테트로미노, Time: 40 ms, Memory: 2880 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> v;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+
+  v.resize(n, vector<int>(m));
+
+  for (int y = 0; y < n; ++y) {
+    for (int x = 0; x < m; ++x) {
+      cin >> v[y][x];
+    }
+  }
+
+  vector<vector<tuple<int, int>>> patterns = {
+      {{0, 0}, {1, 0}, {2, 0}, {3, 0}},  {{0, 0}, {0, 1}, {0, 2}, {0, 3}},
+
+      {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
+
+      {{0, 0}, {1, 0}, {2, 0}, {2, 1}},  {{0, 0}, {1, 0}, {0, 1}, {0, 2}},
+      {{0, 0}, {0, 1}, {1, 1}, {2, 1}},  {{0, 0}, {0, 1}, {0, 2}, {-1, 2}},
+      {{0, 0}, {0, 1}, {-1, 1}, {-2, 1}},  {{0, 0}, {1, 0}, {1, 1}, {1, 2}},
+      {{0, 0}, {0, 1}, {1, 0}, {2, 0}},  {{0, 0}, {0, 1}, {0, 2}, {1, 2}},
+
+      {{0, 0}, {1, 0}, {1, 1}, {2, 1}},  {{0, 0}, {0, 1}, {-1, 1}, {-1, 2}},
+      {{0, 0}, {1, 0}, {0, 1}, {-1, 1}},  {{0, 0}, {0, 1}, {1, 1}, {1, 2}},
+
+      {{0, 0}, {0, 1}, {1, 1}, {0, 2}},  {{0, 0}, {0, 1}, {-1, 1}, {1, 1}},
+      {{0, 0}, {0, 1}, {-1, 1}, {0, 2}}, {{0, 0}, {1, 0}, {1, 1}, {2, 0}},
+  };
+
+  int result = 0;
+
+  for (int y = 0; y < n; ++y) {
+    for (int x = 0; x < m; ++x) { 
+      for (auto& pattern : patterns) {
+        int area = 0;
+
+        for (auto& pos : pattern) {
+          int new_y = y + get<0>(pos);
+          int new_x = x + get<1>(pos);
+
+          if (new_y < 0 || new_y >= n || new_x < 0 || new_x >= m) {
+            continue;
+          }
+
+          area += v[new_y][new_x];
+        }
+
+        result = max(result, area);
+      }
+    }
+  }
+
+  cout << result;
+
+  return 0;
+}
