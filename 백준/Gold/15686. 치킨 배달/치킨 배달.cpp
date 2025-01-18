a7ef9089dba043cce008bf02ec8399a7dcbf99d6
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-18 16:51:00 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2352 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<tuple<int, int>> house;
+vector<tuple<int, int>> chicken;
+set<vector<int>> s;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+
+  for (int y = 0; y < n; ++y) {
+    for (int x = 0; x < n; ++x) {
+      int a;
+      cin >> a;
+
+      if (a == 1) {
+        house.push_back({x, y});
+      } else if (a == 2) {
+        chicken.push_back({x, y});
+      }
+    }
+  }
+
+  vector<int> chicken_indexes(chicken.size(), 0);
+  fill(chicken_indexes.end() - m, chicken_indexes.end(), 1);
+
+  do {
+    vector<int> temp;
+
+    for (int i = 0; i < chicken_indexes.size(); ++i) {
+      if (chicken_indexes[i] == 1) {
+        temp.push_back(i);
+      }
+    }
+
+    s.insert(temp);
+  } while (next_permutation(chicken_indexes.begin(), chicken_indexes.end()));
+
+  int result = INT32_MAX;
+
+  for (auto& v : s) {
+    int sum = 0;
+    for (auto& [house_x, house_y] : house) {
+      int dist = INT32_MAX;
+      for (int i : v) {
+        auto [chicken_x, chicken_y] = chicken[i];
+        int now_dist = abs(house_x - chicken_x) + abs(house_y - chicken_y);
+        if (dist > now_dist) {
+          dist = now_dist;
+        }
+      }
+      sum += dist;
+    }
+
+    if (result > sum) {
+      result = sum;
+    }
+  }
+
+  cout << result;
+
+  return 0;
+}
