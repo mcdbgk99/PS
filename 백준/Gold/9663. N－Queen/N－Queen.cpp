641e6bdc34a5fd79e9853ab7bf49f3f354b05135
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-20 17:48:17 +0900

    [Gold IV] Title: N-Queen, Time: 812 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cpp
+++ 백준/Gold/9663. N－Queen/N－Queen.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 constexpr size_t kBoardSize = 14 * 14;
-deque<tuple<int, bitset<kBoardSize>>> dq;
 
 int main() {
   ios::sync_with_stdio(false);
@@ -11,47 +10,84 @@ int main() {
   int n;
   cin >> n;
 
-  int result = 0;
-  dq.push_back({0, bitset<kBoardSize>()});
+  auto dfs = [&n](int start_x) {
+    int result = 0;
+    deque<tuple<int, bitset<kBoardSize>>> dq;
 
-  while (!dq.empty()) {
-    auto [now_y, now_board] = dq.back();
-    dq.pop_back();
+    bitset<kBoardSize> bs;
+    bs[start_x] = 1;
 
-    if (now_y == n) {
-      ++result;
-      continue;
+    int left = start_x - 1;
+    int right = start_x + 1;
+    for (int new_y = 1; new_y < n; ++new_y) {
+      if (left >= 0) {
+        bs[left + new_y * n] = 1;
+        --left;
+      }
+
+      bs[start_x + new_y * n] = 1;
+
+      if (right < n) {
+        bs[right + new_y * n] = 1;
+        ++right;
+      }
     }
 
-    for (int queen = 0; queen < n; ++queen) {
-      if (now_board[queen + now_y * n]) {
+    dq.push_back({1, bs});
+
+    while (!dq.empty()) {
+      auto [now_y, now_board] = dq.back();
+      dq.pop_back();
+
+      if (now_y == n) {
+        ++result;
         continue;
       }
 
-      bitset<kBoardSize> new_board = now_board;
-      new_board[queen + now_y * n] = 1;
-
-      int left = queen - 1;
-      int right = queen + 1;
-      for (int new_y = now_y + 1; new_y < n; ++new_y) {
-        if (left >= 0) {
-          new_board[left + new_y * n] = 1;
-          --left;
+      for (int queen = 0; queen < n; ++queen) {
+        if (now_board[queen + now_y * n]) {
+          continue;
         }
 
-        new_board[queen + new_y * n] = 1;
+        bitset<kBoardSize> new_board = now_board;
+        new_board[queen + now_y * n] = 1;
 
-        if (right < n) {
-          new_board[right + new_y * n] = 1;
-          ++right;
+        left = queen - 1;
+        right = queen + 1;
+        for (int new_y = now_y + 1; new_y < n; ++new_y) {
+          if (left >= 0) {
+            new_board[left + new_y * n] = 1;
+            --left;
+          }
+
+          new_board[queen + new_y * n] = 1;
+
+          if (right < n) {
+            new_board[right + new_y * n] = 1;
+            ++right;
+          }
         }
+
+        dq.push_back({now_y + 1, new_board});
       }
-
-      dq.push_back({now_y + 1, new_board});
     }
+
+    return result;
+  };
+
+  int result = 0;
+
+  for (int i = 0; i < n / 2; ++i) {
+    result += dfs(i);
   }
 
-  cout << result << "\n";
+  result *= 2;
+
+  if (n % 2 != 0) {
+    result += dfs(n / 2);
+  }
+
+  cout << result;
 
   return 0;
 }
