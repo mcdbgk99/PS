97e3d56fcdcb0022d5dc0834d9772f00cc1d4631
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-23 05:32:46 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 100 ms, Memory: 15052 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+
+  vector<string> v(n);
+
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+  }
+
+  deque<tuple<int, int, int>> dq;
+  vector<vector<int>> patterns = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+  vector<vector<vector<int>>> visited(
+      2, vector<vector<int>>(n, vector<int>(m, -1)));
+
+  visited[0][0][0] = 1;
+  dq.push_back({0, 0, 0});
+
+  while (!dq.empty()) {
+    auto [now_y, now_x, breached] = dq.front();
+    dq.pop_front();
+
+    for (auto& pattern : patterns) {
+      int new_y = now_y + pattern[0];
+      int new_x = now_x + pattern[1];
+
+      if (new_y < 0 || new_y >= n || new_x < 0 || new_x >= m) {
+        continue;
+      }
+
+      int need_breach = v[new_y][new_x] == '1';
+
+      if (visited[need_breach | breached][new_y][new_x] != -1 &&
+          visited[need_breach | breached][new_y][new_x] <=
+              visited[breached][now_y][now_x] + 1) {
+        continue;
+      }
+
+      if (need_breach) {
+        if (breached) {
+          continue;
+        }
+
+        dq.push_back({new_y, new_x, 1});
+      } else {
+        dq.push_back({new_y, new_x, breached});
+      }
+
+      visited[need_breach | breached][new_y][new_x] =
+          visited[breached][now_y][now_x] + 1;
+    }
+  }
+
+  cout << static_cast<int>(
+      min<uint32_t>(visited[0][n - 1][m - 1], visited[1][n - 1][m - 1]));
+
+  return 0;
+}
