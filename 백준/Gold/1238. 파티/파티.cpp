42a891bc9eea397dd4dcb2599992ba1bdace6686
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-23 16:29:25 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2356 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cpp
+++ 백준/Gold/1238. 파티/파티.cpp
@@ -8,58 +8,55 @@ int main() {
   int n, m, x;
   cin >> n >> m >> x;
 
-  vector<vector<tuple<int, int>>> v(n + 1);
+  vector<vector<tuple<int, int>>> v_forward(n + 1);
+  vector<vector<tuple<int, int>>> v_reverse(n + 1);
 
   for (int i = 0; i < m; ++i) {
     int a, b, c;
     cin >> a >> b >> c;
-    v[a].push_back({b, c});
+    v_forward[a].push_back({b, c});
+    v_reverse[b].push_back({a, c});
   }
 
-  vector<int> home_to_party(n + 1, -1);
-  vector<int> party_to_home(n + 1, -1);
-
-  for (int i = 1; i <= n; ++i) {
-    vector<tuple<bool, int>> visited(n + 1, {false, -1});
+  auto dijkstra = [&n](vector<vector<tuple<int, int>>>& v, int start) {
+    vector<int> step(n + 1, INT32_MAX);
     auto cmp = [](tuple<int, int>& left, tuple<int, int>& right) {
       return get<1>(left) > get<1>(right);
     };
     priority_queue<tuple<int, int>, vector<tuple<int, int>>, decltype(cmp)> pq;
-    visited[i] = {false, 0};
-    pq.push({i, 0});
+    step[start] = 0;
+    pq.push({start, 0});
 
     while (!pq.empty()) {
       auto [now_node, now_step] = pq.top();
       pq.pop();
 
-      if (get<0>(visited[now_node])) {
+      if (now_step > step[now_node]) {
         continue;
-      }
-      get<0>(visited[now_node]) = true;
-
-      if (i != x && now_node == x) {
-        home_to_party[i] = now_step;
-      }
-
-      if (i == x && now_node != x) {
-        party_to_home[now_node] = now_step;
       }
 
       for (auto& [new_node, new_step] : v[now_node]) {
-        if (get<1>(visited[new_node]) != -1 &&
-            get<1>(visited[new_node]) < now_step + new_step) {
+        if (step[new_node] <= step[now_node] + new_step) {
           continue;
         }
 
-        get<1>(visited[new_node]) = now_step + new_step;
-        pq.push({new_node, now_step + new_step});
+        step[new_node] = step[now_node] + new_step;
+        pq.push({new_node, step[new_node]});
       }
     }
-  }
+
+    return step;
+  };
+
+  vector<int> home_to_party = dijkstra(v_reverse, x);
+  vector<int> party_to_home = dijkstra(v_forward, x);
 
   int result = 0;
   for (int i = 1; i <= n; ++i) {
-    result = max(result, home_to_party[i] + party_to_home[i]);
+    if (i != x && home_to_party[i] != INT32_MAX &&
+        party_to_home[i] != INT32_MAX) {
+      result = max(result, home_to_party[i] + party_to_home[i]);
+    }
   }
 
   cout << result;
