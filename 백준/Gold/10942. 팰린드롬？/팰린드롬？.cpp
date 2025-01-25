e39b2094a7e052f227b52c641efcc97715ea4ae9
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-25 23:50:35 +0900

    [Gold IV] Title: 팰린드롬?, Time: 16 ms, Memory: 1596 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cpp
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cpp
@@ -1,28 +1,31 @@
 #pragma GCC optimize("O3")
-#include <bits/stdc++.h>
+#include <stdio.h>
 #include <unistd.h>
+
+#include <array>
+#include <vector>
 using namespace std;
 
-inline int readChar();
+inline char readChar();
 template <class T = int>
 inline T readInt();
 inline void writeWord(const char *s);
 static const int buf_size = 1 << 18;
-inline int getChar() {
+inline char getChar() {
   static char buf[buf_size];
   static int len = 0, pos = 0;
   if (pos == len) pos = 0, len = fread(buf, 1, buf_size, stdin);
   if (pos == len) return -1;
   return buf[pos++];
 }
-inline int readChar() {
-  int c = getChar();
+inline char readChar() {
+  char c = getChar();
   while (c <= 32) c = getChar();
   return c;
 }
 template <class T>
 inline T readInt() {
-  int s = 1, c = readChar();
+  char c = readChar();
   T x = 0;
   while ('0' <= c && c <= '9') x = x * 10 + c - '0', c = getChar();
   return x;
@@ -46,12 +49,12 @@ struct Flusher {
   ~Flusher() { flush(); }
 } flusher;
 
-constexpr uint32_t kCompileTimeHash(char *str, uint32_t seed = 0) {
+constexpr unsigned long kCompileTimeHash(char *str, unsigned long seed = 0) {
   return *str ? kCompileTimeHash(str + 1, seed * 88848 + *str) : seed;
 }
-constexpr uint32_t kDateTimeHash = kCompileTimeHash(__DATE__ __TIME__);
-constexpr uint32_t kFileHash = kCompileTimeHash(__FILE__);
-constexpr uint64_t kMakeHash(uint64_t hash) {
+constexpr unsigned long kDateTimeHash = kCompileTimeHash(__DATE__ __TIME__);
+constexpr unsigned long kFileHash = kCompileTimeHash(__FILE__);
+constexpr unsigned long long kMakeHash(unsigned long long hash) {
   hash ^= hash >> 33;
   hash *= 0xff51afd7ed558ccdULL;
   hash ^= hash >> 33;
@@ -59,11 +62,11 @@ constexpr uint64_t kMakeHash(uint64_t hash) {
   hash ^= hash >> 33;
   return hash;
 }
-constexpr uint64_t kHashBase = kMakeHash(kDateTimeHash ^ kFileHash);
+constexpr unsigned long long kHashBase = kMakeHash(kDateTimeHash ^ kFileHash);
 constexpr int kMaxN = 2000;
 
 constexpr auto kMakeHashPower() {
-  array<uint64_t, kMaxN + 2> arr{1};
+  array<unsigned long long, kMaxN + 2> arr{1};
   for (int i = 1; i <= kMaxN; ++i) {
     arr[i] = arr[i - 1] * kHashBase;
   }
@@ -81,8 +84,8 @@ int main() {
     v[i] = readInt();
   }
 
-  vector<uint64_t> hash_prefix(n + 2, 0);
-  vector<uint64_t> hash_suffix(n + 2, 0);
+  vector<unsigned long long> hash_prefix(n + 2, 0);
+  vector<unsigned long long> hash_suffix(n + 2, 0);
 
   for (int i = 1; i <= n; ++i) {
     hash_prefix[i] = hash_prefix[i - 1] * kHashBase + v[i];
@@ -104,10 +107,10 @@ int main() {
       int s_backward = n - e_forward + 1;
       int e_backward = n - s_forward + 1;
 
-      uint64_t hash_forward =
+      unsigned long long hash_forward =
           hash_prefix[e_forward] -
           hash_prefix[s_forward - 1] * kHashPower[e_forward - s_forward + 1];
-      uint64_t hash_backward =
+      unsigned long long hash_backward =
           hash_suffix[e_backward] -
           hash_suffix[s_backward - 1] * kHashPower[e_backward - s_backward + 1];
       writeChar(hash_forward == hash_backward ? '1' : '0');
