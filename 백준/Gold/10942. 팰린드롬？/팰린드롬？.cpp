3d0b587ec89d40de9f8646aaa729e66a618acf15
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-25 20:02:13 +0900

    [Gold IV] Title: 팰린드롬?, Time: 20 ms, Memory: 1776 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cpp
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cpp
@@ -2,167 +2,84 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/////////////////////////////////////////////////////////////////////////////////////////////
-/*
- * Author : jinhan814
- * Date : 2021-05-06
- * Source : https://blog.naver.com/jinhan814/222266396476
- * Description : FastIO implementation for cin, cout.
- */
-constexpr int SZ = 1 << 20;
-
-class INPUT {
- private:
-  char read_buf[SZ];
-  int read_idx, next_idx;
-  bool __END_FLAG__, __GETLINE_FLAG__;
-
- public:
-  explicit operator bool() { return !__END_FLAG__; }
-  bool IsBlank(char c) { return c == ' ' || c == '\n'; }
-  bool IsEnd(char c) { return c == '\0'; }
-  char _ReadChar() {
-    if (read_idx == next_idx) {
-      next_idx = fread(read_buf, sizeof(char), SZ, stdin);
-      if (next_idx == 0) return 0;
-      read_idx = 0;
-    }
-    return read_buf[read_idx++];
-  }
-  char ReadChar() {
-    char ret = _ReadChar();
-    for (; IsBlank(ret); ret = _ReadChar());
-    return ret;
-  }
-  template <typename T>
-  T ReadInt() {
-    T ret = 0;
-    char cur = _ReadChar();
-    bool flag = 0;
-    for (; IsBlank(cur); cur = _ReadChar());
-    if (cur == '-') flag = 1, cur = _ReadChar();
-    for (; !IsBlank(cur) && !IsEnd(cur); cur = _ReadChar())
-      ret = 10 * ret + (cur & 15);
-    if (IsEnd(cur)) __END_FLAG__ = 1;
-    return flag ? -ret : ret;
-  }
-  string ReadString() {
-    string ret;
-    char cur = _ReadChar();
-    for (; IsBlank(cur); cur = _ReadChar());
-    for (; !IsBlank(cur) && !IsEnd(cur); cur = _ReadChar()) ret.push_back(cur);
-    if (IsEnd(cur)) __END_FLAG__ = 1;
-    return ret;
-  }
-  double ReadDouble() {
-    string ret = ReadString();
-    return stod(ret);
-  }
-  string getline() {
-    string ret;
-    char cur = _ReadChar();
-    for (; cur != '\n' && !IsEnd(cur); cur = _ReadChar()) ret.push_back(cur);
-    if (__GETLINE_FLAG__) __END_FLAG__ = 1;
-    if (IsEnd(cur)) __GETLINE_FLAG__ = 1;
-    return ret;
-  }
-  friend INPUT& getline(INPUT& in, string& s) {
-    s = in.getline();
-    return in;
-  }
-} _in;
-
-class OUTPUT {
- private:
-  char write_buf[SZ];
-  int write_idx;
-
- public:
-  ~OUTPUT() { Flush(); }
-  explicit operator bool() { return 1; }
-  void Flush() {
-    fwrite(write_buf, sizeof(char), write_idx, stdout);
-    write_idx = 0;
-  }
-  void WriteChar(char c) {
-    if (write_idx == SZ) Flush();
-    write_buf[write_idx++] = c;
-  }
-  template <typename T>
-  int GetSize(T n) {
-    int ret = 1;
-    for (n = n >= 0 ? n : -n; n >= 10; n /= 10) ret++;
-    return ret;
-  }
-  template <typename T>
-  void WriteInt(T n) {
-    int sz = GetSize(n);
-    if (write_idx + sz >= SZ) Flush();
-    if (n < 0) write_buf[write_idx++] = '-', n = -n;
-    for (int i = sz; i-- > 0; n /= 10) write_buf[write_idx + i] = n % 10 | 48;
-    write_idx += sz;
-  }
-  void WriteString(string s) {
-    for (auto& c : s) WriteChar(c);
-  }
-  void WriteDouble(double d) { WriteString(to_string(d)); }
-} _out;
-
-/* operators */
-INPUT& operator>>(INPUT& in, char& i) {
-  i = in.ReadChar();
-  return in;
+inline int readChar();
+template <class T = int>
+inline T readInt();
+template <class T>
+inline void writeInt(T x, char end = 0);
+inline void writeChar(int x);
+inline void writeWord(const char *s);
+static const int buf_size = 1 << 18;
+inline int getChar() {
+#ifndef LOCAL
+  static char buf[buf_size];
+  static int len = 0, pos = 0;
+  if (pos == len) pos = 0, len = fread(buf, 1, buf_size, stdin);
+  if (pos == len) return -1;
+  return buf[pos++];
+#endif
 }
-INPUT& operator>>(INPUT& in, string& i) {
-  i = in.ReadString();
-  return in;
+inline int readChar() {
+#ifndef LOCAL
+  int c = getChar();
+  while (c <= 32) c = getChar();
+  return c;
+#else
+  char c;
+  cin >> c;
+  return c;
+#endif
 }
-template <typename T, typename std::enable_if_t<is_arithmetic_v<T>>* = nullptr>
-INPUT& operator>>(INPUT& in, T& i) {
-  if constexpr (is_floating_point_v<T>)
-    i = in.ReadDouble();
-  else if constexpr (is_integral_v<T>)
-    i = in.ReadInt<T>();
-  return in;
+template <class T>
+inline T readInt() {
+#ifndef LOCAL
+  int s = 1, c = readChar();
+  T x = 0;
+  if (c == '-') s = -1, c = getChar();
+  while ('0' <= c && c <= '9') x = x * 10 + c - '0', c = getChar();
+  return s == 1 ? x : -x;
+#else
+  T x;
+  cin >> x;
+  return x;
+#endif
 }
-
-OUTPUT& operator<<(OUTPUT& out, char i) {
-  out.WriteChar(i);
-  return out;
+static int write_pos = 0;
+static char write_buf[buf_size];
+inline void writeChar(int x) {
+  if (write_pos == buf_size)
+    fwrite(write_buf, 1, buf_size, stdout), write_pos = 0;
+  write_buf[write_pos++] = x;
 }
-OUTPUT& operator<<(OUTPUT& out, string i) {
-  out.WriteString(i);
-  return out;
+template <class T>
+inline void writeInt(T x, char end) {
+  if (x < 0) writeChar('-'), x = -x;
+  char s[24];
+  int n = 0;
+  while (x || !n) s[n++] = '0' + x % 10, x /= 10;
+  while (n--) writeChar(s[n]);
+  if (end) writeChar(end);
 }
-template <typename T, typename std::enable_if_t<is_arithmetic_v<T>>* = nullptr>
-OUTPUT& operator<<(OUTPUT& out, T i) {
-  if constexpr (is_floating_point_v<T>)
-    out.WriteDouble(i);
-  else if constexpr (is_integral_v<T>)
-    out.WriteInt<T>(i);
-  return out;
+inline void writeWord(const char *s) {
+  while (*s) writeChar(*s++);
 }
-
-/* macros */
-#define fastio 1
-#define cin _in
-#define cout _out
-#define istream INPUT
-#define ostream OUTPUT
-/////////////////////////////////////////////////////////////////////////////////////////////
+struct Flusher {
+  ~Flusher() {
+    if (write_pos) fwrite(write_buf, 1, write_pos, stdout), write_pos = 0;
+  }
+} flusher;
 
 int main() {
   mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
-  uniform_int_distribution<> dist(0x10203040, 0x50607080);
+  uniform_int_distribution<> dist(0x12341234, 0x56785678);
   uint64_t hash_base = dist(gen);
 
-  int n;
-  cin >> n;
+  int n = readInt();
 
   vector<int> v(n + 1);
 
   for (int i = 1; i <= n; ++i) {
-    cin >> v[i];
+    v[i] = readInt();
   }
 
   vector<uint64_t> hash_prefix(n + 2, 0);
@@ -175,12 +92,11 @@ int main() {
     hash_suffix[i] = hash_suffix[i - 1] * hash_base + v[n - i + 1];
   }
 
-  int m;
-  cin >> m;
+  int m = readInt();
 
   for (int i = 0; i < m; ++i) {
-    int s_forward, e_forward;
-    cin >> s_forward >> e_forward;
+    int s_forward = readInt();
+    int e_forward = readInt();
     int s_backward = n - e_forward + 1;
     int e_backward = n - s_forward + 1;
 
@@ -190,7 +106,12 @@ int main() {
     uint64_t hash_backward =
         hash_suffix[e_backward] -
         hash_suffix[s_backward - 1] * hash_power[e_backward - s_backward + 1];
-    cout << (hash_forward == hash_backward) << '\n';
+    if (hash_forward == hash_backward) {
+      writeChar('1');
+    } else {
+      writeChar('0');
+    }
+    writeChar('\n');
   }
 
   return 0;
