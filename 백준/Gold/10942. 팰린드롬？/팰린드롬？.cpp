87f62ae564ea25093e7a4dfd004f2856e360017f
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-26 02:28:58 +0900

    [Gold IV] Title: 팰린드롬?, Time: 16 ms, Memory: 3516 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cpp
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cpp
@@ -8,13 +8,13 @@
 using namespace std;
 
 inline char readChar();
-template <class T = int>
+template <class T = size_t>
 inline T readInt();
 inline void writeWord(const char *s);
-static const int buf_size = 1 << 18;
+static const size_t buf_size = 1 << 18;
 static char buf[buf_size];
 inline char getChar() {
-  static int len = 0, pos = 0;
+  static size_t len = 0, pos = 0;
   if (pos == len) pos = 0, len = fread(buf, 1, buf_size, stdin);
   if (pos == len) return -1;
   return buf[pos++];
@@ -31,7 +31,7 @@ inline T readInt() {
   while ('0' <= c && c <= '9') x = x * 10 + c - '0', c = getChar();
   return x;
 }
-constexpr int OUT_BUF_SIZE = 1 << 16;
+constexpr size_t OUT_BUF_SIZE = 1 << 21;
 static char out_buf[OUT_BUF_SIZE];
 static size_t out_pos = 0;
 inline void flush() {
@@ -39,10 +39,7 @@ inline void flush() {
   write(STDOUT_FILENO, out_buf, out_pos);
   out_pos = 0;
 }
-inline void writeChar(char c) {
-  if (out_pos == OUT_BUF_SIZE) flush();
-  out_buf[out_pos++] = c;
-}
+inline void writeChar(char c) { out_buf[out_pos++] = c; }
 inline void writeWord(const char *s) {
   while (*s) writeChar(*s++);
 }
@@ -64,7 +61,7 @@ constexpr unsigned long long kMakeHash(unsigned long long hash) {
   return hash;
 }
 constexpr unsigned long long kHashBase = kMakeHash(kDateTimeHash ^ kFileHash);
-constexpr int kMaxN = 2000;
+constexpr size_t kMaxN = 2000;
 
 constexpr auto kMakeHashPower() {
   array<unsigned long long, kMaxN + 2> arr{1};
@@ -76,31 +73,31 @@ constexpr auto kMakeHashPower() {
 constexpr auto kHashPower = kMakeHashPower();
 
 int main() {
-  const int n = readInt();
+  const size_t n = readInt();
 
   array<int, kMaxN + 1> v{};
 
   [[assume(n >= 1 && n <= 2000)]];
-  for (int i = 1; i <= n; ++i) {
+  for (size_t i = 1; i <= n; ++i) {
     v[i] = readInt();
   }
 
   array<unsigned long long, kMaxN + 2> hash_prefix{};
   array<unsigned long long, kMaxN + 2> hash_suffix{};
 
-  for (int i = 1; i <= n; ++i) {
+  for (size_t i = 1; i <= n; ++i) {
     hash_prefix[i] = hash_prefix[i - 1] * kHashBase + v[i];
     hash_suffix[i] = hash_suffix[i - 1] * kHashBase + v[n - i + 1];
   }
 
-  const int m = readInt();
+  const size_t m = readInt();
 
   [[assume(m >= 1 && m <= 1000000)]];
-  for (int i = 0; i < m; ++i) {
-    int s_forward = readInt();
-    int e_forward = readInt();
-    int s_backward = n - e_forward + 1;
-    int e_backward = n - s_forward + 1;
+  for (size_t i = 0; i < m; ++i) {
+    size_t s_forward = readInt();
+    size_t e_forward = readInt();
+    size_t s_backward = n - e_forward + 1;
+    size_t e_backward = n - s_forward + 1;
 
     unsigned long long hash_forward =
         hash_prefix[e_forward] -
@@ -111,6 +108,8 @@ int main() {
     writeChar(hash_forward == hash_backward ? '1' : '0');
     writeChar('\n');
   }
+
+  flush();
 
   return 0;
 }
