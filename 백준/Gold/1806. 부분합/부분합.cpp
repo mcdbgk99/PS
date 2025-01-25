b0f32e4cc93822fe39d35c40285c95858e2329d7
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-25 10:49:23 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2476 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, s;
+  cin >> n >> s;
+
+  vector<int> v(n);
+
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+  }
+
+  int result = INT32_MAX;
+  int sum = v[0];
+  int left = 0;
+  int right = 0;
+
+  while (left < n && right < n) {
+    if (sum < s) {
+      ++right;
+      sum += v[right];
+    } else {
+      result = min(result, right - left);
+      sum -= v[left];
+      ++left;
+    }
+  }
+
+  if (result != INT32_MAX) {
+    cout << result + 1;
+  } else {
+    cout << 0;
+  }
+
+  return 0;
+}
