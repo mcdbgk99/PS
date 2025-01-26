e7a9202315f680cd600f803d108bf4ea74d1c464
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-26 21:55:44 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 52 ms, Memory: 10288 KB -BaekjoonHub

---
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int kMax = 1000000 + 1;
+array<int, kMax> divs{};
+array<int, kMax> muls{};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int> v(n);
+  bitset<kMax> bs;
+
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+    bs[v[i]] = 1;
+  }
+
+  for (int i = 1; i < kMax; ++i) {
+    if (!bs[i]) {
+      continue;
+    }
+
+    for (int j = i; j < kMax; j += i) {
+      ++divs[j];
+    }
+  }
+
+  for (int i = 1; i < kMax; ++i) {
+    if (!bs[i]) {
+      continue;
+    }
+
+    for (int j = i * 2; j < kMax; j += i) {
+      if (!bs[j]) {
+        continue;
+      }
+
+      ++muls[i];
+    }
+  }
+
+  for (int i : v) {
+    cout << muls[i] - divs[i] + 1 << " ";
+  }
+
+  return 0;
+}
