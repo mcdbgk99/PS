8a180cf8b637d5e8eb0cbdab23e55be48e7900d7
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-27 00:31:11 +0900

    [Gold III] Title: ACM Craft, Time: 28 ms, Memory: 2440 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.cpp
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cpp
@@ -1,32 +1,84 @@
-#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+#include <stdio.h>
+
+#include <deque>
+#include <vector>
 using namespace std;
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+inline int readChar();
+template <class T = int>
+inline T readInt();
+template <class T>
+inline void writeInt(T x, char end = 0);
+inline void writeChar(int x);
+inline void writeWord(const char *s);
+static const int buf_size = 1 << 18;
+inline int getChar() {
+  static char buf[buf_size];
+  static int len = 0, pos = 0;
+  if (pos == len) pos = 0, len = fread(buf, 1, buf_size, stdin);
+  if (pos == len) return -1;
+  return buf[pos++];
+}
+inline int readChar() {
+  int c = getChar();
+  while (c <= 32) c = getChar();
+  return c;
+}
+template <class T>
+inline T readInt() {
+  int s = 1, c = readChar();
+  T x = 0;
+  if (c == '-') s = -1, c = getChar();
+  while ('0' <= c && c <= '9') x = x * 10 + c - '0', c = getChar();
+  return s == 1 ? x : -x;
+}
+static int write_pos = 0;
+static char write_buf[buf_size];
+inline void writeChar(int x) {
+  if (write_pos == buf_size)
+    fwrite(write_buf, 1, buf_size, stdout), write_pos = 0;
+  write_buf[write_pos++] = x;
+}
+template <class T>
+inline void writeInt(T x, char end) {
+  if (x < 0) writeChar('-'), x = -x;
+  char s[24];
+  int n = 0;
+  while (x || !n) s[n++] = '0' + x % 10, x /= 10;
+  while (n--) writeChar(s[n]);
+  if (end) writeChar(end);
+}
+inline void writeWord(const char *s) {
+  while (*s) writeChar(*s++);
+}
+struct Flusher {
+  ~Flusher() {
+    if (write_pos) fwrite(write_buf, 1, write_pos, stdout), write_pos = 0;
+  }
+} flusher;
 
-  int t;
-  cin >> t;
+int main() {
+  int t = readInt();
 
   while (t--) {
-    int n, k;
-    cin >> n >> k;
+    int n = readInt();
+    int k = readInt();
 
     vector<int> costs(n + 1);
     vector<vector<int>> nodes(n + 1, vector<int>());
 
     for (int i = 1; i <= n; ++i) {
-      cin >> costs[i];
+      costs[i] = readInt();
     }
 
     for (int i = 0; i < k; ++i) {
-      int x, y;
-      cin >> x >> y;
+      int x = readInt();
+      int y = readInt();
       nodes[y].push_back(x);
     }
 
-    int w;
-    cin >> w;
+    int w = readInt();
 
     vector<int> dp(n + 1, -1);
     deque<tuple<int, bool>> dq;
@@ -55,7 +107,7 @@ int main() {
       }
     }
 
-    cout << dp[w] << "\n";
+    writeInt(dp[w], '\n');
   }
 
   return 0;
