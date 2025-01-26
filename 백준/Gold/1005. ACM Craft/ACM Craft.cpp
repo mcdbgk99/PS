2b1c3b0708f874d2eebb009904b8394c497b7eed
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-27 02:24:02 +0900

    [Gold III] Title: ACM Craft, Time: 128 ms, Memory: 2892 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.cpp
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cpp
@@ -1,120 +1,61 @@
-#pragma GCC optimize("O3")
-#include <stdio.h>
-
-#include <algorithm>
-#include <deque>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-inline int readChar();
-template <class T = int>
-inline T readInt();
-template <class T>
-inline void writeInt(T x, char end = 0);
-inline void writeChar(int x);
-inline void writeWord(const char *s);
-static const int buf_size = 1 << 22;
-inline int getChar() {
-  static char buf[buf_size];
-  static int len = 0, pos = 0;
-  if (pos == len) pos = 0, len = fread(buf, 1, buf_size, stdin);
-  if (pos == len) return -1;
-  return buf[pos++];
-}
-inline int readChar() {
-  int c = getChar();
-  while (c <= 32) c = getChar();
-  return c;
-}
-template <class T>
-inline T readInt() {
-  int s = 1, c = readChar();
-  T x = 0;
-  if (c == '-') s = -1, c = getChar();
-  while ('0' <= c && c <= '9') x = x * 10 + c - '0', c = getChar();
-  return s == 1 ? x : -x;
-}
-static int write_pos = 0;
-static char write_buf[buf_size];
-inline void writeChar(int x) {
-  if (write_pos == buf_size)
-    fwrite(write_buf, 1, buf_size, stdout), write_pos = 0;
-  write_buf[write_pos++] = x;
-}
-template <class T>
-inline void writeInt(T x, char end) {
-  if (x < 0) writeChar('-'), x = -x;
-  char s[24];
-  int n = 0;
-  while (x || !n) s[n++] = '0' + x % 10, x /= 10;
-  while (n--) writeChar(s[n]);
-  if (end) writeChar(end);
-}
-inline void writeWord(const char *s) {
-  while (*s) writeChar(*s++);
-}
-struct Flusher {
-  ~Flusher() {
-    if (write_pos) fwrite(write_buf, 1, write_pos, stdout), write_pos = 0;
-  }
-} flusher;
-
-vector<int> costs;
-vector<vector<short>> nodes;
-vector<short> in_degree;
-vector<int> result;
-
 int main() {
-  int t = readInt();
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
 
   while (t--) {
-    short n = readInt<short>();
-    int k = readInt();
+    int n, k;
+    cin >> n >> k;
 
-    costs.resize(n + 1);
-    nodes.assign(n + 1, vector<short>());
-    in_degree.assign(n + 1, 0);
-    result.assign(n + 1, 0);
+    vector<int> costs(n + 1);
+    vector<vector<int>> nodes(n + 1, vector<int>());
 
     for (int i = 1; i <= n; ++i) {
-      costs[i] = readInt();
+      cin >> costs[i];
     }
 
     for (int i = 0; i < k; ++i) {
-      short x = readInt<short>();
-      short y = readInt<short>();
-      nodes[x].push_back(y);
-      in_degree[y]++;
+      int x, y;
+      cin >> x >> y;
+      nodes[y].push_back(x);
     }
 
-    short w = readInt<short>();
+    int w;
+    cin >> w;
 
-    deque<short> dq;
-    for (int i = 1; i <= n; ++i) {
-      if (in_degree[i] == 0) {
-        dq.push_back(i);
-        result[i] = costs[i];
-      }
-    }
+    vector<int> dp(n + 1, -1);
+    deque<tuple<int, bool>> dq;
+
+    dq.push_back({w, false});
 
     while (!dq.empty()) {
-      short now_node = dq.front();
-      dq.pop_front();
+      auto [now_node, now_built] = dq.back();
+      dq.pop_back();
 
-      if (now_node == w) {
-        break;
-      }
+      if (now_built) {
+        int max_cost = 0;
+        for (int new_node : nodes[now_node]) {
+          max_cost = max(max_cost, dp[new_node]);
+        }
+        dp[now_node] = max_cost + costs[now_node];
+      } else {
+        if (dp[now_node] != -1) {
+          continue;
+        }
 
-      for (int new_node : nodes[now_node]) {
-        result[new_node] =
-            max(result[new_node], result[now_node] + costs[new_node]);
-        if (--in_degree[new_node] == 0) {
-          dq.push_back(new_node);
+        dq.push_back({now_node, true});
+        for (int i = nodes[now_node].size() - 1; i >= 0; --i) {
+          dq.push_back({nodes[now_node][i], false});
         }
       }
     }
 
-    writeInt(result[w], '\n');
+    cout << dp[w] << "\n";
   }
 
   return 0;
