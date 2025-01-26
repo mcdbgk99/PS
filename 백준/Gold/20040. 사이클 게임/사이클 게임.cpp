5936b0783799ebca87398a6010318a7eb9233268
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-26 17:47:03 +0900

    [Gold IV] Title: 사이클 게임, Time: 12 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.cpp
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cpp
@@ -1,105 +1,119 @@
-#pragma GCC optimize("O3")
-#pragma comment(linker, "/STACK:336777216")
-#include <stdio.h>
+#pragma GCC optimize("O3,unroll-loops")
+#include <sys/mman.h>
 #include <unistd.h>
-
-#include <vector>
 using namespace std;
 
-inline char readChar();
-template <class T = size_t>
-inline T readInt();
-template <class T>
-inline void writeInt(T x, char end = 0);
-inline void writeWord(const char* s);
-static const size_t buf_size = 1 << 17;
-static char buf[buf_size];
-inline char getChar() {
-  static size_t len = 0, pos = 0;
-  if (pos == len) pos = 0, len = fread(buf, 1, buf_size, stdin);
-  if (pos == len) return -1;
-  return buf[pos++];
+namespace FastIO {
+char* input_ptr;
+__attribute__((constructor)) void init() {
+  input_ptr = (char*)mmap(0, 1 << 23, PROT_READ, MAP_PRIVATE, 0, 0);
 }
-inline char readChar() {
-  char c = getChar();
-  while (c <= 32) c = getChar();
-  return c;
+
+inline void scan(int& x) {
+  asm volatile(
+      "xor %%eax, %%eax\n"  // x = 0
+      "mov %1, %%rsi\n"     // 64-bit 포인터 로드
+      // 숫자가 아닌 문자 건너뛰기
+      "1:\n"
+      "movb (%%rsi), %%cl\n"
+      "cmp $0x30, %%cl\n"
+      "jge 2f\n"
+      "inc %%rsi\n"
+      "jmp 1b\n"
+      // 첫 번째 숫자 처리
+      "2:\n"
+      "movzbl (%%rsi), %%ecx\n"
+      "inc %%rsi\n"
+      "sub $0x30, %%ecx\n"
+      "add %%ecx, %%eax\n"
+      // 나머지 숫자 처리
+      "3:\n"
+      "movzbl (%%rsi), %%ecx\n"
+      "cmp $0x30, %%ecx\n"
+      "jl 4f\n"
+      "inc %%rsi\n"
+      "sub $0x30, %%ecx\n"
+      "imul $10, %%eax\n"
+      "add %%ecx, %%eax\n"
+      "jmp 3b\n"
+      "4:\n"
+      "mov %%rsi, %1\n"  // 포인터 업데이트
+      "mov %%eax, %0\n"  // 결과 저장
+      : "=m"(x), "+m"(input_ptr)
+      :
+      : "rax", "rcx", "rsi", "memory");
 }
-template <class T>
-inline T readInt() {
-  char c = readChar();
-  T x = 0;
-  while ('0' <= c && c <= '9') x = x * 10 + c - '0', c = getChar();
+}  // namespace FastIO
+
+constexpr int MAX_N = 1000001;
+alignas(64) int points[MAX_N];
+
+inline int Find(int x) {
+  asm volatile(
+      "mov %0, %%eax\n"
+      "1:\n"
+      "mov (points)(,%%eax,4), %%ebx\n"
+      "test %%ebx, %%ebx\n"
+      "js 2f\n"
+      "mov (points)(,%%ebx,4), %%ecx\n"
+      "test %%ecx, %%ecx\n"
+      "js 3f\n"
+      "mov %%ecx, (points)(,%%eax,4)\n"
+      "3:\n"
+      "mov %%ebx, %%eax\n"
+      "jmp 1b\n"
+      "2:\n"
+      : "=a"(x)
+      : "0"(x)
+      : "ebx", "ecx", "memory");
   return x;
 }
-constexpr size_t OUT_BUF_SIZE = 1 << 8;
-static char out_buf[OUT_BUF_SIZE];
-static size_t out_pos = 0;
-inline void flush() {
-  if (out_pos == 0) return;
-  write(STDOUT_FILENO, out_buf, out_pos);
-  out_pos = 0;
-}
-inline void writeChar(char c) { out_buf[out_pos++] = c; }
-inline void writeWord(const char* s) {
-  while (*s) writeChar(*s++);
-}
-template <class T>
-inline void writeInt(T x, char end) {
-  if (x < 0) writeChar('-'), x = -x;
-  char s[24];
-  int n = 0;
-  while (x || !n) s[n++] = '0' + x % 10, x /= 10;
-  while (n--) writeChar(s[n]);
-  if (end) writeChar(end);
-}
-struct Flusher {
-  ~Flusher() { flush(); }
-} flusher;
 
-inline void Union(vector<int>& points, int a, int b) {
-  if (points[a] > points[b]) {
-    swap(a, b);
-  }
-
-  points[a] += points[b];
-  points[b] = a;
-}
-
-inline int Find(vector<int>& points, int x) {
-  while (points[x] >= 0) {
-    int p = points[x];
-    if (points[p] >= 0) {
-      points[x] = points[p];
-    }
-    x = p;
-  }
-  return x;
+inline void Union(int a, int b) {
+  asm volatile(
+      "mov %0, %%eax\n"
+      "mov %1, %%ebx\n"
+      "mov (points)(,%%eax,4), %%ecx\n"
+      "mov (points)(,%%ebx,4), %%edx\n"
+      "cmp %%ecx, %%edx\n"
+      "jle 1f\n"
+      "xchg %%eax, %%ebx\n"
+      "1:\n"
+      "add %%edx, %%ecx\n"
+      "mov %%ecx, (points)(,%%eax,4)\n"
+      "mov %%eax, (points)(,%%ebx,4)\n"
+      :
+      : "r"(a), "r"(b)
+      : "eax", "ebx", "ecx", "edx", "memory");
 }
 
 int main() {
-  int n = readInt();
-  int m = readInt();
+  int n, m;
+  FastIO::scan(n);
+  FastIO::scan(m);
 
-  vector<int> points(n, -1);
-
-  for (int i = 1; i <= m; i++) {
-    int a = readInt();
-    int b = readInt();
-    a = Find(points, a);
-    b = Find(points, b);
-
-    if (a == b) {
-      writeInt(i);
-      flush();
-      return 0;
-    }
-
-    Union(points, a, b);
+  for (int i = 0; i <= n; ++i) {
+    points[i] = -1;
   }
 
-  writeChar('0');
-  flush();
+  for (int i = 1; i <= m; ++i) {
+    int a, b;
+    FastIO::scan(a);
+    FastIO::scan(b);
+    a = Find(a);
+    b = Find(b);
 
+    if (a == b) {
+      char num[10];
+      char* p = num + 9;
+      do *--p = '0' + i % 10;
+      while (i /= 10);
+      write(1, p, num + 9 - p);
+      return 0;
+    }
+    Union(a, b);
+  }
+
+  write(1, "0", 1);
   return 0;
 }
