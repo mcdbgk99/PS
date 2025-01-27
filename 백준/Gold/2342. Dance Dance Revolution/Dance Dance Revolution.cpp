1222c3ed263057bd26afe6ca51db221a873c9341
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-27 12:43:39 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 12 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cpp
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cpp
@@ -1,47 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+constexpr auto kMakeCostTable() {
+  array<array<int, 5>, 5> arr{};
+
+  for (int a = 0; a < 5; ++a) {
+    for (int b = 0; b < 5; ++b) {
+      if (a == b) {
+        arr[a][b] = 1;
+      } else if (a == 0) {
+        arr[a][b] = 2;
+      } else if ((a + b) & 0x1) {
+        arr[a][b] = 3;
+      } else {
+        arr[a][b] = 4;
+      }
+    }
+  }
+
+  return arr;
+};
+constexpr auto kCostTable = kMakeCostTable();
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  vector<int> v;
-  v.reserve(100000);
-
-  int x;
-  while (cin >> x && x != 0) {
-    v.push_back(x);
-  }
-
-  if (v.empty()) {
-    cout << '0';
-    return 0;
-  }
-
-  auto calculate_cost = [](int a, int b) {
-    if (a == b) {
-      return 1;
-    }
-
-    if (a == 0) {
-      return 2;
-    }
-
-    if ((a + b) & 0x1) {
-      return 3;
-    }
-
-    return 4;
-  };
-
-  array<array<int, 5>, 5> now_dp;
-  array<array<int, 5>, 5> new_dp;
+  array<array<int, 5>, 5> now_dp, new_dp;
   for (int i = 0; i < 5; ++i) {
     now_dp[i].fill(INT32_MAX);
   }
   now_dp[0][0] = 0;
 
-  for (int i : v) {
+  int x;
+  while (cin >> x && x != 0) {
     for (int i = 0; i < 5; ++i) {
       new_dp[i].fill(INT32_MAX);
     }
@@ -52,17 +44,17 @@ int main() {
           continue;
         }
 
-        if (left != i) {
-          int cost = calculate_cost(right, i);
-          if (new_dp[left][i] > now_dp[left][right] + cost) {
-            new_dp[left][i] = now_dp[left][right] + cost;
+        if (left != x) {
+          int cost = kCostTable[right][x];
+          if (new_dp[left][x] > now_dp[left][right] + cost) {
+            new_dp[left][x] = now_dp[left][right] + cost;
           }
         }
 
-        if (right != i) {
-          int cost = calculate_cost(left, i);
-          if (new_dp[i][right] > now_dp[left][right] + cost) {
-            new_dp[i][right] = now_dp[left][right] + cost;
+        if (right != x) {
+          int cost = kCostTable[left][x];
+          if (new_dp[x][right] > now_dp[left][right] + cost) {
+            new_dp[x][right] = now_dp[left][right] + cost;
           }
         }
       }
@@ -71,14 +63,7 @@ int main() {
     swap(now_dp, new_dp);
   }
 
-  int result = INT32_MAX;
-  for (int i = 0; i < 5; ++i) {
-    for (int j = 0; j < 5; ++j) {
-      result = min(result, now_dp[i][j]);
-    }
-  }
-
-  cout << result;
+  cout << *min_element(&now_dp[0][0], &now_dp[0][0] + 25);
 
   return 0;
 }
