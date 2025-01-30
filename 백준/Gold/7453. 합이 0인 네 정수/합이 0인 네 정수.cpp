eca8c0049406d9672b2e56f5cd5e6350886a5e4b
mcdbgk99 <mcdbgk99@gmail.com>
2025-01-31 02:37:01 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 9008 ms, Memory: 631952 KB -BaekjoonHub

---
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+
+  vector<int64_t> a(n), b(n), c(n), d(n);
+
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i] >> b[i] >> c[i] >> d[i];
+  }
+
+  unordered_map<int64_t, int> s_ab;
+  s_ab.reserve(n * n);
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      ++s_ab[a[i] + b[j]];
+    }
+  }
+
+  int64_t result = 0;
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      auto it = s_ab.find(0 - (c[i] + d[j]));
+      if (it != s_ab.end()) {
+        result += it->second;
+      }
+    }
+  }
+
+  cout << result;
+
+  return 0;
+}
