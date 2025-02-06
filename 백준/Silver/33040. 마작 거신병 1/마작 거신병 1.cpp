f3e099bb074d1568c9c6cf98d371440bbfe5ea82
mcdbgk99 <mcdbgk99@gmail.com>
2025-02-06 17:07:24 +0900

    [Silver I] Title: 마작 거신병 1, Time: 4 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Silver/33040. 마작 거신병 1/마작 거신병 1.cpp
+++ 백준/Silver/33040. 마작 거신병 1/마작 거신병 1.cpp
@@ -8,44 +8,18 @@ int main() {
   int h, w, c, d;
   cin >> h >> w >> c >> d;
 
-  vector<int> v(h, w);
-  vector<int> forward(h, 0), backward(h, 0);
-  backward.back() = v.back();
-
-  for (int i = 1; i < h; ++i) {
-    int temp = v[i - 1] + forward[i - 1] * 8 - v[i];
-    forward[i] = (temp < 0 ? 0 : temp / 8 + 1);
-    if (forward[i] > v[i]) {
-      cout << -1;
-      return 0;
-    }
-  }
-
-  for (int i = h - 2; i >= 0; --i) {
-    int temp = v[i + 1] + backward[i + 1] * 8 - 1 - v[i];
-    if (temp < 0) {
-      cout << -1;
-      return 0;
-    }
-    backward[i] = min(v[i], temp / 8);
-    if (forward[i] > backward[i]) {
-      cout << -1;
-      return 0;
-    }
-  }
-
-  int forward_sum = accumulate(forward.begin(), forward.end(), 0);
-  int backward_sum = accumulate(backward.begin(), backward.end(), 0);
-
-  if (d < forward_sum || d > backward_sum) {
+  int min_nine = h * (h - 1) / 2;
+  if (d < min_nine || d > h * w - min_nine) {
     cout << -1;
     return 0;
   }
 
-  int remain = d - forward_sum;
+  vector<int> v(h);
+  iota(v.begin(), v.end(), 0);
+  int remain = d - min_nine;
   for (int i = h - 1; i >= 0; --i) {
-    int add = min(remain, backward[i] - forward[i]);
-    forward[i] += add;
+    int add = min(remain, w - h + 1 + i - v[i]);
+    v[i] += add;
     remain -= add;
     if (remain <= 0) {
       break;
@@ -53,11 +27,11 @@ int main() {
   }
 
   for (int i = 0; i < h; ++i) {
-    for (int j = 0; j < forward[i]; ++j) {
+    for (int j = 0; j < v[i]; ++j) {
       cout << "9 ";
     }
 
-    for (int j = 0; j < v[i] - forward[i]; ++j) {
+    for (int j = 0; j < w - v[i]; ++j) {
       cout << "1 ";
     }
 
