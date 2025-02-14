8d177691883f727f5a6c81da7b5a479a5d8ae792
mcdbgk99 <mcdbgk99@gmail.com>
2025-02-14 17:22:28 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.cpp
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cpp
@@ -5,20 +5,11 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int n;
-  cin >> n;
-
-  if (n >= 90 && n <= 100) {
-    cout << "A";
-  } else if (n >= 80 && n <= 89) {
-    cout << "B";
-  } else if (n >= 70 && n <= 79) {
-    cout << "C";
-  } else if (n >= 60 && n <= 69) {
-    cout << "D";
-  } else {
-    cout << "F";
-  }
-
+  int64_t score;
+  cin >> score;
+  cout << (score < 60 ? 'F'
+                      : "DCBA"[(((score - 60) * 0xCCCCCCCD) >> 35) > 3
+                                   ? 3
+                                   : (((score - 60) * 0xCCCCCCCD) >> 35)]);
   return 0;
 }
