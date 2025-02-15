e84b9900a5871693537c8945c44df68eca8979ad
mcdbgk99 <mcdbgk99@gmail.com>
2025-02-15 19:35:48 +0900

    [Platinum I] Title: 마지막 요세푸스 문제, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Platinum/1179. 마지막 요세푸스 문제/마지막 요세푸스 문제.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n, k;
+  cin >> n >> k;
+
+  if (k == 1) {
+    cout << n;
+    return 0;
+  }
+
+  deque<tuple<int64_t, int64_t>> dq;
+
+  while (n >= k) {
+    dq.push_back({n, n % k});
+    n -= n / k;
+  }
+
+  int64_t result = 0;
+  for (int64_t i = 2; i <= n; ++i) {
+    result = (result + k) % i;
+  }
+
+  while (!dq.empty()) {
+    auto [s, r] = dq.back();
+    dq.pop_back();
+
+    result -= r;
+    if (result < 0) {
+      result += s;
+    } else {
+      result += (result / (k - 1));
+    }
+  }
+
+  cout << result + 1;
+
+  return 0;
+}
