4edf72c6204ea1263d5262800a1fcf647b490da8
mcdbgk99 <mcdbgk99@gmail.com>
2025-02-19 09:07:39 +0900

    [Silver IV] Title: 팬케익 먹기, Time: 0 ms, Memory: 2084 KB, Score: 3 point -BaekjoonHub

--- 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.cpp
+++ 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.cpp
@@ -13,9 +13,12 @@ int main() {
     int64_t d;
     cin >> cake[0] >> cake[1] >> cake[2] >> d;
 
-    while (d--) {
-      --(*max_element(cake.begin(), cake.end()));
-    }
+    sort(cake.begin(), cake.end());
+    int64_t sum = accumulate(cake.begin(), cake.end(), 0ll) - d;
+    cake[0] = min(sum / 3, cake[0]);
+    sum -= cake[0];
+    cake[1] = min(sum / 2, cake[1]);
+    cake[2] = sum - cake[1];
 
     cout << cake[0] * cake[1] * cake[2] << "\n";
   }
