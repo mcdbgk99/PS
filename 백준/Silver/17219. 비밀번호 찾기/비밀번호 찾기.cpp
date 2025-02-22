3bf5350c50fbd0be2eb965dd2ef466cdb89176ca
mcdbgk99 <mcdbgk99@gmail.com>
2025-02-23 01:09:32 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 96 ms, Memory: 15612 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cpp
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-unordered_map<string, string> passwords;
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
   int n, m;
   cin >> n >> m;
+
+  unordered_map<string, string> passwords;
+  passwords.reserve(n);
 
   for (int i = 0; i < n; ++i) {
     string url, password;
